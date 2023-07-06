253723883315da2d01c9c847b3dbfeb4d87a8270
Vladimir Zuev <vzuev@natera.com>
2023-07-06 17:46:12 +0200

    init

---
+++ rules/okta_policy_name_rule.go
@@ -0,0 +1,87 @@
+package rules
+
+import (
+	"fmt"
+
+	"github.com/terraform-linters/tflint-plugin-sdk/hclext"
+	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+)
+
+// OktaPolicyNameRule checks whether ...
+type OktaPolicyNameRule struct {
+	tflint.DefaultRule
+	resourceType  string
+	attributeName string
+	max           int
+	min           int
+}
+
+// NewOktaPolicyNameRule returns a new rule
+func NewOktaPolicyNameRule() *OktaPolicyNameRule {
+	return &OktaPolicyNameRule{
+		resourceType:  "okta_auth_server_policy",
+		attributeName: "name",
+		max:           50,
+		min:           1,
+	}
+}
+
+// Name returns the rule name
+func (r *OktaPolicyNameRule) Name() string {
+	return "okta_policy_name_rule"
+}
+
+// Enabled returns whether the rule is enabled by default
+func (r *OktaPolicyNameRule) Enabled() bool {
+	return true
+}
+
+// Severity returns the rule severity
+func (r *OktaPolicyNameRule) Severity() tflint.Severity {
+	return tflint.ERROR
+}
+
+// Link returns the rule reference link
+func (r *OktaPolicyNameRule) Link() string {
+	return ""
+}
+
+// Check checks whether ...
+func (r *OktaPolicyNameRule) Check(runner tflint.Runner) error {
+	resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
+		Attributes: []hclext.AttributeSchema{{Name: r.attributeName}},
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, resource := range resources.Blocks {
+		attribute, exists := resource.Body.Attributes[r.attributeName]
+		if !exists {
+			continue
+		}
+
+		err := runner.EvaluateExpr(attribute.Expr, func(val string) error {
+			if len(val) > r.max {
+				runner.EmitIssue(
+					r,
+					fmt.Sprintf(`Name must be from 1 to 50 characters`),
+					attribute.Expr.Range(),
+				)
+			}
+			if len(val) < r.min {
+				runner.EmitIssue(
+					r,
+					fmt.Sprintf(`Name must be from 1 to 50 characters`),
+					attribute.Expr.Range(),
+				)
+			}
+			return nil
+		}, nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ rules/okta_policy_name_rule_test.go
@@ -0,0 +1,83 @@
+package rules
+
+import (
+	"testing"
+
+	hcl "github.com/hashicorp/hcl/v2"
+	"github.com/terraform-linters/tflint-plugin-sdk/helper"
+)
+
+func Test_OktaPolicyNameRule_OneChar(t *testing.T) {
+	tests := []struct {
+		Name     string
+		Content  string
+		Expected helper.Issues
+	}{
+		{
+			Name: "Length is 1",
+			Content: `
+resource "okta_auth_server_policy" "auditlogservice_read" {
+  name = "f"
+}`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "Length is 50",
+			Content: `
+resource "okta_auth_server_policy" "auditlogservice_read" {
+  name = "Check ____________________________________ 50 char"
+}`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "Length is 0",
+			Content: `
+resource "okta_auth_server_policy" "auditlogservice_read" {
+  name = ""
+}`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewOktaPolicyNameRule(),
+					Message: "Name must be from 1 to 50 characters",
+					Range: hcl.Range{
+						Filename: "resource.tf",
+						Start:    hcl.Pos{Line: 3, Column: 10},
+						End:      hcl.Pos{Line: 3, Column: 12},
+					},
+				},
+			},
+		},
+		{
+			Name: "name must be from 1 to 50 characters",
+			Content: `
+resource "okta_auth_server_policy" "auditlogservice_read" {
+  name = "Check _____________________________________ 51 char"
+}`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewOktaPolicyNameRule(),
+					Message: "Name must be from 1 to 50 characters",
+					Range: hcl.Range{
+						Filename: "resource.tf",
+						Start:    hcl.Pos{Line: 3, Column: 10},
+						End:      hcl.Pos{Line: 3, Column: 63},
+					},
+				},
+			},
+		},
+	}
+
+	rule := NewOktaPolicyNameRule()
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			runner := helper.TestRunner(t, map[string]string{"resource.tf": test.Content})
+
+			if err := rule.Check(runner); err != nil {
+				t.Fatalf("Unexpected error occurred: %s", err)
+			}
+
+			helper.AssertIssues(t, test.Expected, runner.Issues)
+		})
+	}
+}
